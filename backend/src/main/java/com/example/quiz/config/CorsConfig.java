9f48f3deb1dc4d7efad7ea057bd98332fdf2e9d9
Rain <xy19536173168@163.com>
2026-01-12 02:26:29 +0800

    修改后端跨域配置类

--- backend/src/main/java/com/example/quiz/config/CorsConfig.java
+++ backend/src/main/java/com/example/quiz/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**")  // 所有接口
-                .allowedOrigins("http://localhost:5173") // 允许的源
+                .allowedOrigins("*") // 允许的源
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 允许的方法
                 .allowedHeaders("*") // 允许的请求头
                 .allowCredentials(true) // 允许携带凭证（如cookies）
