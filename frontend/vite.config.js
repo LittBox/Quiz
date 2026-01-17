16963678a45df31b0d08b0dc6d24b6b3d59ee67a
Rain <xy19536173168@163.com>
2026-01-17 23:35:21 +0800

    恢复http

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -20,7 +20,7 @@ export default defineConfig({
         proxy: {
             // 匹配所有以 /api 开头的请求，全部转发到你的后端服务器
             '/api': {
-                target: 'https://123.207.43.214:8443', // 你的后端真实地址（不用改）
+                target: 'http://123.207.43.214:8080', // 你的后端真实地址（不用改）
                 changeOrigin: true, // 必须开启：解决跨域问题，模拟请求源一致
                 secure: false,      // 必须开启：忽略后端SSL证书不可信的问题 ✔️ 核心解决你的报错
                 rewrite: (path) => path.replace(/^\/api/, '') // 必须配置：把请求里的/api去掉再转发
